68e21fed59ed30c9540a94cb3a78c216a3194120
junlongchen0829 <junlong.chen@studio.unibo.it>
2026-02-18 23:46:09 +0100

    Update mmseqs2_commands.sh

--- preprocessing/mmseqs2_commands.sh
+++ preprocessing/mmseqs2_commands.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
+# MMseqs2 redundancy removal (run separately for each dataset)
+# Thresholds required by the course: 30% identity and 40% coverage
+
+mmseqs createdb INPUT.fasta DB
+mmseqs cluster DB CLU TMP --min-seq-id 0.3 -c 0.4 --cov-mode 0
+mmseqs createseqfiledb DB CLU REPDB
+mmseqs result2flat DB DB REPDB OUTPUT_rep.fasta --use-fasta-header
 
